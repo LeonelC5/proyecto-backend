bfa3f4d8c185625c75bffb791661ade95f332ac4
LeonelC5 <112716144+LeonelC5@users.noreply.github.com>
2023-11-21 15:23:23 -0500

    Update SpringSecurity.java

--- demo/src/main/java/com/example/demo/Security/SpringSecurity.java
+++ demo/src/main/java/com/example/demo/Security/SpringSecurity.java
@@ -25,29 +25,26 @@ public class SpringSecurity {
     }
 
     @Bean
-public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.csrf().disable()
-        .authorizeRequests(authorize ->
-            authorize
-                .antMatchers("/register/**", "/index").permitAll()
-                .antMatchers("/users").hasRole("ADMIN")
-                .antMatchers("/lugares/**").permitAll()
-        )
-        .formLogin(
-            form -> form
-                .loginPage("/login")
-                .loginProcessingUrl("/login")
-                .defaultSuccessUrl("/users")
-                .permitAll()
-        )
-        .logout(
-            logout -> logout
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .permitAll()
-        );
-
-    return http.build();
-}
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) ->
+                        authorize.requestMatchers("/register/**").permitAll()
+                                .requestMatchers("/index").permitAll()
+                                .requestMatchers("/users").hasRole("ADMIN")
+                                .requestMatchers("/lugares/**").permitAll()
+                ).formLogin(
+                        form -> form
+                                .loginPage("/login")
+                                .loginProcessingUrl("/login")
+                                .defaultSuccessUrl("/users")
+                                .permitAll()
+                ).logout(
+                        logout -> logout
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                                .permitAll()
+                );
+        return http.build();
+    }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
