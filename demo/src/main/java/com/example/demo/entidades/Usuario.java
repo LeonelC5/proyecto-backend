d82c761d61dc672f8cbfb27e6aa2aa0eef3a53ee
LeonelC5 <112716144+LeonelC5@users.noreply.github.com>
2023-11-21 13:21:47 -0500

    Update Usuario.java

--- demo/src/main/java/com/example/demo/entidades/Usuario.java
+++ demo/src/main/java/com/example/demo/entidades/Usuario.java
@@ -1,5 +1,7 @@
 package com.example.demo.entidades;
 import jakarta.persistence.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -22,20 +24,31 @@ public class Usuario {
     @Column(name = "correo")
     private String correo;
 
+    @Column(nullable=false)
+    private String password;
+
     @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
     private List<Reserva> reservas;
 
     @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
     private List<Mascota> mascotas;
 
+    @ManyToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL)
+    @JoinTable(
+            name="users_roles",
+            joinColumns={@JoinColumn(name="USER_ID", referencedColumnName="ID")},
+            inverseJoinColumns={@JoinColumn(name="ROLE_ID", referencedColumnName="ID")})
+    private List<Roles> roles = new ArrayList<>();
+
     public Usuario() {
     }
 
-    public Usuario(String nombre, String apellido, String telefono, String correo) {
+    public Usuario(String nombre, String apellido, String telefono, String correo, String password) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.telefono = telefono;
         this.correo = correo;
+        this.password = password;
     }
 
     public Long getIdUsuario() {
@@ -92,5 +105,25 @@ public class Usuario {
 
     public void setMascotas(List<Mascota> mascotas) {
         this.mascotas = mascotas;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Roles> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Roles> roles) {
+        this.roles = roles;
+    }
+
+    public CharSequence get() {
+        return null;
     }
 }
