84aa6f4c6f0b96111d14484fcad3d3d9663a2f8e
LeonelC5 <112716144+LeonelC5@users.noreply.github.com>
2023-11-21 13:22:29 -0500

    Update UsuarioRepository.java

--- demo/src/main/java/com/example/demo/repositorios/UsuarioRepository.java
+++ demo/src/main/java/com/example/demo/repositorios/UsuarioRepository.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entidades.Usuario;
+import com.example.demo.entidades.UsuarioDTO;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     List<Usuario> findByNombre(String nombre);
+    Usuario findByCorreo(String correo);
+    List<UsuarioDTO> findAllUsers();
 }
