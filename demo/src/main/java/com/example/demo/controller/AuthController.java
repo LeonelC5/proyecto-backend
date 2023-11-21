f766172f7f3fb85338eea7332ec12ff93f9ae3f7
LeonelC5 <112716144+LeonelC5@users.noreply.github.com>
2023-11-21 15:41:30 -0500

    Update AuthController.java

--- demo/src/main/java/com/example/demo/controller/AuthController.java
+++ demo/src/main/java/com/example/demo/controller/AuthController.java
@@ -1,45 +1,45 @@
-package com.example.demo.controller;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import jakarta.validation.Valid;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestMapping;
+// package com.example.demo.controller;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.stereotype.Controller;
+// import jakarta.validation.Valid;
+// import org.springframework.validation.BindingResult;
+// import org.springframework.web.bind.annotation.ModelAttribute;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.demo.entidades.UsuarioDTO;
-import com.example.demo.servicios.UsuarioService;
+// import com.example.demo.entidades.UsuarioDTO;
+// import com.example.demo.servicios.UsuarioService;
 
-@Controller
-@RequestMapping
-public class AuthController {
-    private UsuarioService usuarioService;
+// @Controller
+// @RequestMapping
+// public class AuthController {
+//     private UsuarioService usuarioService;
 
-    public AuthController(UsuarioService usuarioService) {
-        this.usuarioService = usuarioService;
-    }
+//     public AuthController(UsuarioService usuarioService) {
+//         this.usuarioService = usuarioService;
+//     }
 
-    @PostMapping("/register/save")
-    public ResponseEntity<String> registration(@Valid @ModelAttribute("user") UsuarioDTO usuarioDTO,
-                                                BindingResult result) {
-        UsuarioDTO existingUser = usuarioService.findByEmail(usuarioDTO.getCorreo());
+//     @PostMapping("/register/save")
+//     public ResponseEntity<String> registration(@Valid @ModelAttribute("user") UsuarioDTO usuarioDTO,
+//                                                 BindingResult result) {
+//         UsuarioDTO existingUser = usuarioService.findByEmail(usuarioDTO.getCorreo());
 
-        if (existingUser != null && existingUser.getCorreo() != null && !existingUser.getCorreo().isEmpty()) {
-            return ResponseEntity.badRequest().body("Ya existe una cuenta registrada con el mismo correo");
-        }
+//         if (existingUser != null && existingUser.getCorreo() != null && !existingUser.getCorreo().isEmpty()) {
+//             return ResponseEntity.badRequest().body("Ya existe una cuenta registrada con el mismo correo");
+//         }
 
-        if (result.hasErrors()) {
-            return ResponseEntity.badRequest().body("Error en los datos del formulario");
-        }
+//         if (result.hasErrors()) {
+//             return ResponseEntity.badRequest().body("Error en los datos del formulario");
+//         }
 
-        usuarioService.saveUser(usuarioDTO);
-        return ResponseEntity.ok("Registro exitoso");
-    }
+//         usuarioService.saveUser(usuarioDTO);
+//         return ResponseEntity.ok("Registro exitoso");
+//     }
 
-    // Endpoint para el inicio de sesión
-      // @PostMapping("/login")
-    // public ResponseEntity<String> login(@RequestParam String correo, @RequestParam String password) {
-    //     return ResponseEntity.ok("Inicio de sesión exitoso");
-    // }
-}
+//     Endpoint para el inicio de sesión
+//     @PostMapping("/login")
+//     public ResponseEntity<String> login(@RequestParam String correo, @RequestParam String password) {
+//         return ResponseEntity.ok("Inicio de sesión exitoso");
+//     }
+// }
